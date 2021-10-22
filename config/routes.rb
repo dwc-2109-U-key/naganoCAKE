b9ddc8a84270b5dcfa0c7d906253ab3f968e3b1e
“Kimura-Taiki” <taiguimucun850@gmail.com>
2021-10-22 14:03:06 +0000

    rails g admin/Itemsで諸ページを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   
+  namespace :admin do
+    get 'items/new'
+    get 'items/index'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
   # namespace :public do
   #   get 'orders/new'
   #   get 'orders/index'
