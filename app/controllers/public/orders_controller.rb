49f1951fc44ec89c4167ebae8a23dbe863a51ef9
“Kimura-Taiki” <taiguimucun850@gmail.com>
2021-10-25 08:35:32 +0000

    注文詳細まで

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -33,6 +33,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def confirm
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { not_paid: 0,  paid: 1, producting: 2, shipping: 3, shipped: 4 }
 end
